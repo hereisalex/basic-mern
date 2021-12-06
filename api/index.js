9605f0c5bf6f33232d3b728daf775d1c6b11f46e
Alex McKenzie <here.is.alex@gmail.com>
2021-12-05 21:19:05 -0600

    implement hosting on heroku

--- api/index.js
+++ api/index.js
@@ -1,36 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const userRoute = require('./routes/users');
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
 
-//dotenv file keeps our sensitive information hidden
 dotenv.config();
-//connect to our mongodb database
-mongoose.connect(process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("Connected to MongoDB");
-    }
-);
 
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-//parse incoming requests
 app.use(express.json());
-app.use(helmet());
-app.use(morgan('common'));
-//routes
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+app.use(morgan("common"));
 
-//this is our express middleware
-app.listen(8800, () => {
-    console.log("Backend server is running on port 8800!");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Backend server is running!");
+});
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,81 +1,74 @@
-const router = require('express').Router();
-const Post = require('../models/Post');
+const router = require("express").Router();
+const Post = require("../models/Post");
 const User = require("../models/User");
 
 //create a post
 
 router.post("/", async (req, res) => {
-    const newPost = new Post(req.body)
-    try {
-        const savedPost = await newPost.save();
-        res.status(200).json(savedPost);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
-
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //update a post
 
 router.put("/:id", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        //make sure the user is the owner of the post
-        if(post.userId === req.body.userId){
-            await post.updateOne({$set:req.body});
-            res.status(200).json("Post updated");
-        }else{
-            res.status(403).json("You can only update your own posts!");
-        }
-    } catch(err) {
-        res.status(500).json(err)
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 //delete a post
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        //make sure the user is the owner of the post
-        if(post.userId === req.body.userId){
-            await post.deleteOne();
-            res.status(200).json("Post deleted");
-        }else{
-            res.status(403).json("You can only delete your own posts!");
-        }
-    } catch(err) {
-        res.status(500).json(err)
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+//like / dislike a post
 
-//like a post
-
-router.put("/:id/like", async (req,res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        //if user is not already in the likes array, add them
-        if(!post.likes.includes(req.body.userId)){
-            await post.updateOne({$push: {likes: req.body.userId}});
-            res.status(200).json("Post liked");
-        } else { //if user already in likes array, remove them
-            await post.updateOne({$pull: {likes: req.body.userId}});
-            res.status(200).json("Post unliked");
-        }
-    } catch(err) {
-        res.status(500).json(err)
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 //get a post
 
 router.get("/:id", async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(post);
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get timeline posts
 
@@ -88,7 +81,19 @@ router.get("/timeline/:userId", async (req, res) => {
         return Post.find({ userId: friendId });
       })
     );
-    res.status(200).json(userPosts.concat(...friendPosts))
+    res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get user's all posts
+
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
